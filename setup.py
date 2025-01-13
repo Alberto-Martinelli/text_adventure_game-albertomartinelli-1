905fa539b11a4bf723bf153f43c32af1bec51f1f
Alberto Martinelli s329095 <s329095@studenti.polito.it>
2025-01-13 17:18:11 +0100

    Created setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+  name='text-based adventure game',
+  version='1.0',
+  author='Alberto-Martinelli',
+  license='MIT',
+  long_description=open('README.md').read(),
+)
